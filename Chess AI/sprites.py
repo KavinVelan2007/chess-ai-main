9a9b95f5b8a9ea4c27602d8b1f6c3b04af374d4d
useless-coder-person <139415197+useless-coder-person@users.noreply.github.com>
2023-07-18 15:40:02 +0000

    commit

--- Chess AI/sprites.py
+++ Chess AI/sprites.py
@@ -4,16 +4,16 @@ w = WIDTH // COLS
 h = HEIGHT // ROWS
 
 sprites = {
-	('black','pawn'): pygame.transform.scale(pygame.image.load('assets/black_pawn.png'),(w,h)),
-	('black','rook'): pygame.transform.scale(pygame.image.load('assets/black_rook.png'),(w,h)),
-	('black','knight'): pygame.transform.scale(pygame.image.load('assets/black_knight.png'),(w,h)),
-	('black','bishop'): pygame.transform.scale(pygame.image.load('assets/black_bishop.png'),(w,h)),
-	('black','king'): pygame.transform.scale(pygame.image.load('assets/black_king.png'),(w,h)),
-	('black','queen'): pygame.transform.scale(pygame.image.load('assets/black_queen.png'),(w,h)),
-	('white','pawn'): pygame.transform.scale(pygame.image.load('assets/white_pawn.png'),(w,h)),
-	('white','rook'): pygame.transform.scale(pygame.image.load('assets/white_rook.png'),(w,h)),
-	('white','knight'): pygame.transform.scale(pygame.image.load('assets/white_knight.png'),(w,h)),
-	('white','bishop'): pygame.transform.scale(pygame.image.load('assets/white_bishop.png'),(w,h)),
-	('white','king'): pygame.transform.scale(pygame.image.load('assets/white_king.png'),(w,h)),
-	('white','queen'): pygame.transform.scale(pygame.image.load('assets/white_queen.png'),(w,h))
+	('black','pawn'): pygame.transform.scale(pygame.image.load('Chess AI/assets/black_pawn.png'),(w,h)),
+	('black','rook'): pygame.transform.scale(pygame.image.load('Chess AI/assets/black_rook.png'),(w,h)),
+	('black','knight'): pygame.transform.scale(pygame.image.load('Chess AI/assets/black_knight.png'),(w,h)),
+	('black','bishop'): pygame.transform.scale(pygame.image.load('Chess AI/assets/black_bishop.png'),(w,h)),
+	('black','king'): pygame.transform.scale(pygame.image.load('Chess AI/assets/black_king.png'),(w,h)),
+	('black','queen'): pygame.transform.scale(pygame.image.load('Chess AI/assets/black_queen.png'),(w,h)),
+	('white','pawn'): pygame.transform.scale(pygame.image.load('Chess AI/assets/white_pawn.png'),(w,h)),
+	('white','rook'): pygame.transform.scale(pygame.image.load('Chess AI/assets/white_rook.png'),(w,h)),
+	('white','knight'): pygame.transform.scale(pygame.image.load('Chess AI/assets/white_knight.png'),(w,h)),
+	('white','bishop'): pygame.transform.scale(pygame.image.load('Chess AI/assets/white_bishop.png'),(w,h)),
+	('white','king'): pygame.transform.scale(pygame.image.load('Chess AI/assets/white_king.png'),(w,h)),
+	('white','queen'): pygame.transform.scale(pygame.image.load('Chess AI/assets/white_queen.png'),(w,h))
 }
